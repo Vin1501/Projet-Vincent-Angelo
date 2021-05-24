ad125ede60419744f3e17dd105a485a056f654f6
Vin1501 <84719612+Vin1501@users.noreply.github.com>
2021-05-24 17:45:28 +0200

    Update Projet d'informatique Vincent Breton Angelo Mazzella.py

--- Projet d'informatique Vincent Breton Angelo Mazzella.py
+++ Projet d'informatique Vincent Breton Angelo Mazzella.py
@@ -14,27 +14,35 @@ import numpy as np
 
 
 #On demande donc à l'utilisateur la taille du tableau souhaité, ainsi que le nombre de jetons qu'il faudra aligner pour l'emporter 
-Abcisse = int(input("Veuillez entrer la taille de l'axe des abscisses souhaitée" ))
-Ordonnée = int(input ("Veuillez entrer la taille de l'axe des ordonnées souhaitée"))
+Colonnestableau = int(input("Veuillez entrer le nombre de colonnes du tableau souhaité" ))
+Lignestableau = int(input ("Veuillez entrer le nombre de lignes du tableau souhaités"))
 Victoire = int(input("Veuillez entrer le nombre de jetons que vous souhaitez voir alignés pour gagner"))
 
-#NB : il est important de mettre les valeurs demandée sous forme d'entiers, sinon Python les considère comme des string. La toute première erreur de code du projet.
+#NB : il est important de mettre les valeurs demandée sous forme d'entiers, sinon Python les considère comme des chaines de caractères. La toute première erreur de code du projet.
 
 def createur_tableau():
-    tableau = np.zeros((Abcisse, Ordonnée))
+    tableau = np.zeros((Lignestableau, Colonnestableau))
     return tableau
 
+#A noter : la "1ere" ligne de la matrice créé est libelée comme étant la 0è
+
 def lacher_jeton(tableau, ligne, col, jeton):
-    tableau[ligne][col] == jeton
+    tableau[ligne][col] = jeton
+    #cette fonction change le tableau à l'emplacement joué en remplaçant le 0 par un "1" ou un "2" qui symbolise les couleurs rouges ou jaunes
+
 def Espace_valide(tableau, col):
-    return tableau [Abcisse-1,col] == 0
+    return tableau [int(0)][col] == 0
+#cette fonction renvoie True si l'on peut jouer dans la colonne demandée par le joueur. 
+
+
 def chercher_prochaine_ligne_ouverte(tableau, col):
-    for l in range(Abcisse):
+    for l in reversed(range(Lignestableau)):
         if tableau[l][col] == 0:
             return l
+#cette fonction a pour rôle de déterminer la ligne ou placer le jeton. On utilise "reversed" range pour partir du bas du tableau
 
-A = createur_tableau()
-print("Le tableau créé est :",A)
+tableau1 = createur_tableau()
+print("Le tableau créé est :",tableau1)
 
 #Le jeu doit faire le différence entre le tour du joueur 1 et celui du joueur 2
 Tour = 0
@@ -46,24 +54,25 @@ jeu_terminé = False
 while not jeu_terminé:
     #demander le choix du joueur 1
     if Tour == 0:
-        col = int(input('Joueur 1 faites votre choix entre 0 et' + (str(Abcisse-1))) 
-        if Espace_valide(tableau,col) :
-            ligne = chercher_prochaine_ligne_ouverte(tableau,col)
-            lacher_jeton(tableau, ligne, col, 1)
+        colonnejouée = int(input('Joueur 1 faites votre choix entre 0 et' + (str(Colonnestableau-1))))
+        if Espace_valide(tableau1,colonnejouée)==True:
+            lignejouée = chercher_prochaine_ligne_ouverte(tableau1,colonnejouée)
+            lacher_jeton(tableau1, lignejouée, colonnejouée, 1)
             
         
         
         
         #demander le choix joueur 2
     else: 
-        col = int(input('Joueur 2 faites votre choix entre 0 et ' + str(Abcisse-1)))
-        if Espace_valide(tableau,col):
-            ligne = chercher_prochaine_ligne_ouverte(tableau,col)
-            lacher_jeton(tableau, ligne, col, 2)
+        col = int(input('Joueur 2 faites votre choix entre 0 et ' + str(Lignestableau-1)))
+        if Espace_valide(tableau1,colonnejouée)==True:
+            lignejouée = chercher_prochaine_ligne_ouverte(tableau1,colonnejouée)
+            lacher_jeton(tableau1, lignejouée, colonnejouée, 2)
     
     #alterner entre tour du joueur 1 et tour du joueur 2 avec le reste de la division euclidienne
     Tour +=1
     Tour = Tour % 2
     
-    print(tableau)
+    print(tableau1)
+            
             
