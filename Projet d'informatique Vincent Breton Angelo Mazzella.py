53dfd74090a2c07ab2777040197a7557ef007ed4
Vin1501 <84719612+Vin1501@users.noreply.github.com>
2021-05-25 09:13:28 +0200

    Update Projet d'informatique Vincent Breton Angelo Mazzella.py

--- Projet d'informatique Vincent Breton Angelo Mazzella.py
+++ Projet d'informatique Vincent Breton Angelo Mazzella.py
@@ -3,7 +3,7 @@
 #Préambule : 
 #Nous avons choisi de travailler sur l'un de projets proposés sur le site de Mr Xavier Dupré, en apportant notre propre version nuancée.
 #Il s'agira de coder le très fameux jeu de puissance 4, mais de telle manière à ce que l'utilisateur joueur puisse jouer à des variations du classique jeu de société.
-#Le but est de permettre à l'utilisateur de choisir lui-même le nombre de points qu'il faut aligner pour l'emporter, ainsi que de choisir la taille de la fente dans laquelle l'on fait coulisser les jetons.
+#Le but est de permettre à l'utilisateur de choisir lui-même la taille de la fente dans laquelle l'on fait coulisser les jetons.
 #Ceci permettra au joueur d'explorer plusieurs dynamiques de jeu, créera un nouvreau jeu qui permettra à l'utilisateur d'exprimer sa créativité en choisissant ses propres règles. #
 
 
@@ -39,7 +39,36 @@ def chercher_prochaine_ligne_ouverte(tableau, col):
     for l in reversed(range(Lignestableau)):
         if tableau[l][col] == 0:
             return l
-#cette fonction a pour rôle de déterminer la ligne ou placer le jeton. On utilise "reversed" range pour partir du bas du tableau
+        #cette fonction a pour rôle de déterminer la ligne ou placer le jeton. On utilise "reversed" range pour partir du bas du tableau
+        
+
+
+def jeton_gagnant(tableau, jeton):
+	# Chercher les alignements horizontaux
+	for c in range(Colonnestableau-3):
+		for l in range(Lignestableau):
+			if tableau[l][c] == jeton and tableau[l][c+1] == jeton and tableau[l][c+2] == jeton and tableau[l][c+3] == jeton:
+				return True
+
+	# Chercher les alignements verticaux
+	for c in range(Colonnestableau):
+		for l in range(Lignestableau-3):
+			if tableau[l][c] == jeton and tableau[l+1][c] == jeton and tableau[l+2][c] == jeton and tableau[l+3][c] == jeton:
+				return True
+
+	# Chercher les alignements diagonaux à pente positive
+	for c in range(Colonnestableau-3):
+		for l in range(Lignestableau-3):
+			if tableau[l][c] == jeton and tableau[l+1][c+1] == jeton and tableau[l+2][c+2] == jeton and tableau[l+3][c+3] == jeton:
+				return True
+
+	# # Chercher les alignements diagonaux à pente négative
+	for c in range(Colonnestableau-3):
+		for l in range(3, Lignestableau):
+			if tableau[l][c] == jeton and tableau[l-1][c+1] == jeton and tableau[l-2][c+2] == jeton and tableau[l-3][c+3] == jeton:
+				return True
+
+
 
 tableau1 = createur_tableau()
 print("Le tableau créé est :",tableau1)
@@ -51,7 +80,7 @@ Tour = 0
 
 jeu_terminé = False 
 
-while not jeu_terminé:
+while jeu_terminé==False:
     #demander le choix du joueur 1
     if Tour == 0:
         colonnejouée = int(input('Joueur 1 faites votre choix entre 0 et' + (str(Colonnestableau-1))))
@@ -59,7 +88,11 @@ while not jeu_terminé:
             lignejouée = chercher_prochaine_ligne_ouverte(tableau1,colonnejouée)
             lacher_jeton(tableau1, lignejouée, colonnejouée, 1)
             
-        
+        if jeton_gagnant(tableau1,1):
+            jeu_terminé==True
+            print("Le joueur 1 a gagné")
+            break
+            
         
         
         #demander le choix joueur 2
@@ -68,11 +101,16 @@ while not jeu_terminé:
         if Espace_valide(tableau1,colonnejouée)==True:
             lignejouée = chercher_prochaine_ligne_ouverte(tableau1,colonnejouée)
             lacher_jeton(tableau1, lignejouée, colonnejouée, 2)
+            
+        if jeton_gagnant(tableau1,2):
+            jeu_terminé==True
+            print("Le joueur 2 a gagné")
+            break
+            
     
     #alterner entre tour du joueur 1 et tour du joueur 2 avec le reste de la division euclidienne
     Tour +=1
     Tour = Tour % 2
     
     print(tableau1)
-            
             
